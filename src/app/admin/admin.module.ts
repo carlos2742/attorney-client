2ec6306d0b7734697b898e2a62547350acb206bf
Carlos <carlosm@yzag.com>
2019-09-02 22:17:49 -0400

    add upload image example

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,13 +4,19 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AdminComponent } from './admin.component';
+import {ngfModule} from 'angular-file';
+import {UploadService} from './services/upload.service';
 
 
 @NgModule({
   declarations: [DashboardComponent, AdminComponent],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    ngfModule
+  ],
+  providers: [
+    UploadService
   ]
 })
 export class AdminModule { }
