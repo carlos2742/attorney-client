9ee94b0674a968f072187f364b04bf4fc3d1cd7f
carlos <carlosm880530@gmail.com>
2019-12-01 17:28:56 -0500

    remove uploadServices from admin

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,7 +5,6 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AdminComponent } from './admin.component';
 import {ngfModule} from 'angular-file';
-import {UploadService} from './services/upload.service';
 import { LoginComponent } from './components/login/login.component';
 import {SharedModule} from '../shared/shared.module';
 import { AdminInputComponent } from './helpers/form/admin-input/admin-input.component';
