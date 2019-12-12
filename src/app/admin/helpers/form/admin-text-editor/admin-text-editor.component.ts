07e77beee053bd10082d03ee7024d55953d66066
carlos <carlosm880530@gmail.com>
2019-12-12 11:56:28 -0500

    remove froala mark

--- src/app/admin/helpers/form/admin-text-editor/admin-text-editor.component.ts
+++ src/app/admin/helpers/form/admin-text-editor/admin-text-editor.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ViewChild} from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
@@ -20,20 +20,24 @@ export class AdminTextEditorComponent implements OnInit {
       heightMin: 300,
       placeholderText: this.placeholder,
       events: {
-        'contentChanged': function(){
-          console.log('--------------------------- Froala Content Changed Event ---------------------------');
-          console.log(this.html.get());
+        'contentChanged': () => {
+          const parsedValue = this.parseValue(this.field.value);
+          this.field.setValue(parsedValue);
         }
       }
     }
   }
 
-  ngOnInit() {
-    this.field.valueChanges.subscribe(
-      value => {
-        console.log('--------------------------- Form Control Value Changed Event ---------------------------');
-        console.log(value);
-      }
-    );
+  ngOnInit() { }
+
+  private parseValue(value){
+    const element = document.createElement('div');
+    element.innerHTML = value;
+    const list = element.getElementsByTagName('p');
+    const lastElement = list[list.length-1];
+    if(lastElement.innerText === 'Powered by Froala Editor'){
+      lastElement.remove()
+    }
+    return element.innerHTML;
   }
 }
