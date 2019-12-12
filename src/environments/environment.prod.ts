6054efcfd6bd2ab0989983777190b151df6b2dc2
carlos <carlosm880530@gmail.com>
2019-12-12 14:08:54 -0500

    set apiUrl to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://attorney-api-prod.herokuapp.com/',
-  apiUrl: 'https://attorney-api-staging.herokuapp.com/'
+  apiUrl: 'https://attorney-api-prod.herokuapp.com/',
+  // apiUrl: 'https://attorney-api-staging.herokuapp.com/'
 };
